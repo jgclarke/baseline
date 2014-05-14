d0070e8adf20f321d292788f87836632fd3ef27c
jgclarke <jgc@jasonclarke.org>
2014-05-14 13:56:20 -0700

    Added content div to enable proproper use of main element

--- archive.php
+++ archive.php
@@ -10,66 +10,70 @@ get_header(); ?>
 	<main id="main" class="site-main site-content" role="main">
 	
 		<div id="primary" class="content-area">
+		
+			<div id="content">
 
-			<?php if ( have_posts() ) : ?>
-	
-				<header class="page-header">
-					<h1 class="page-title">
+				<?php if ( have_posts() ) : ?>
+		
+					<header class="page-header">
+						<h1 class="page-title">
+							<?php
+								if ( is_category() ) :
+									single_cat_title();
+		
+								elseif ( is_tag() ) :
+									single_tag_title();
+		
+								elseif ( is_author() ) :
+									printf( __( 'Author: %s', 'baseline' ), '<span class="vcard">' . get_the_author() . '</span>' );
+		
+								elseif ( is_day() ) :
+									printf( __( 'Day: %s', 'baseline' ), '<span>' . get_the_date() . '</span>' );
+		
+								elseif ( is_month() ) :
+									printf( __( 'Month: %s', 'baseline' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'baseline' ) ) . '</span>' );
+		
+								elseif ( is_year() ) :
+									printf( __( 'Year: %s', 'baseline' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'baseline' ) ) . '</span>' );
+		
+								else :
+									_e( 'Archives', 'baseline' );
+		
+								endif;
+							?>
+						</h1>
 						<?php
-							if ( is_category() ) :
-								single_cat_title();
-	
-							elseif ( is_tag() ) :
-								single_tag_title();
-	
-							elseif ( is_author() ) :
-								printf( __( 'Author: %s', 'baseline' ), '<span class="vcard">' . get_the_author() . '</span>' );
-	
-							elseif ( is_day() ) :
-								printf( __( 'Day: %s', 'baseline' ), '<span>' . get_the_date() . '</span>' );
-	
-							elseif ( is_month() ) :
-								printf( __( 'Month: %s', 'baseline' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'baseline' ) ) . '</span>' );
-	
-							elseif ( is_year() ) :
-								printf( __( 'Year: %s', 'baseline' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'baseline' ) ) . '</span>' );
-	
-							else :
-								_e( 'Archives', 'baseline' );
-	
+							// Show an optional term description.
+							$term_description = term_description();
+							if ( ! empty( $term_description ) ) :
+								printf( '<div class="taxonomy-description">%s</div>', $term_description );
 							endif;
 						?>
-					</h1>
-					<?php
-						// Show an optional term description.
-						$term_description = term_description();
-						if ( ! empty( $term_description ) ) :
-							printf( '<div class="taxonomy-description">%s</div>', $term_description );
-						endif;
-					?>
-				</header><!-- .page-header -->
-	
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-	
-					<?php
-						get_template_part( 'content', get_post_format() );
-					?>
-	
-				<?php endwhile; ?>
-	
-				<?php baseline_paging_nav(); ?>
-	
-			<?php else : ?>
-	
-				<?php get_template_part( 'content', 'none' ); ?>
-	
-			<?php endif; ?>
+					</header><!-- .page-header -->
+		
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+		
+						<?php
+							get_template_part( 'content', get_post_format() );
+						?>
+		
+					<?php endwhile; ?>
+		
+					<?php baseline_paging_nav(); ?>
+		
+				<?php else : ?>
+		
+					<?php get_template_part( 'content', 'none' ); ?>
+		
+				<?php endif; ?>
+			
+			</div><!-- #content -->
 
 		</div><!-- #primary -->
 		
+		<?php get_sidebar(); ?>
+		
 	</main><!-- #main -->
-	
-	<?php get_sidebar(); ?>
 		
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -10,30 +10,34 @@ get_header(); ?>
 	<main id="main" class="site-main site-content" role="main">
 	
 		<div id="primary" class="content-area">
+		
+			<div id="content">
 
-			<?php if ( have_posts() ) : ?>
-	
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-	
-					<?php
-						get_template_part( 'content', 'page' );
-					?>
-	
-				<?php endwhile; ?>
-				
-				<?php baseline_paging_nav(); ?>
-	
-			<?php else : ?>
-	
-				<?php get_template_part( 'content', 'none' ); ?>
-	
-			<?php endif; ?>
+				<?php if ( have_posts() ) : ?>
+		
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+		
+						<?php
+							get_template_part( 'content', 'page' );
+						?>
+		
+					<?php endwhile; ?>
+					
+					<?php baseline_paging_nav(); ?>
+		
+				<?php else : ?>
+		
+					<?php get_template_part( 'content', 'none' ); ?>
+		
+				<?php endif; ?>
+			
+			</div><!-- #content -->
 		
 		</div><!-- #primary -->
+		
+		<?php get_sidebar(); ?>
 
 	</main><!-- #main -->
-	
-	<?php get_sidebar(); ?>
 	
 <?php get_footer(); ?>
